64165715575438bd340c41ac374166481ce1bc40
jeseon <jeseon@gmail.com>
2016-10-12 04:03:28 +0000

    .gitignore 추가

---
+++ src/main/java/sort/Selection.java
@@ -0,0 +1,26 @@
+package sort;
+
+import java.util.Arrays;
+
+public class Selection {
+	public static void main(String[] args) {
+		int tmp, minIndex;
+		int[] arr = {3, 5, 9, 2, 6, 8, 7, 1, 4};
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			minIndex = i;
+			
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+			
+			tmp = arr[i];
+			arr[i] = arr[minIndex];
+			arr[minIndex] = tmp;
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	}
+}
